56d68f70c90ad8fc9170d239e9282282e93b4ae0
Ирина <Smolenskaya2002@yandex.ru>
2023-05-21 23:35:19 +0300

    Create task0101.java

---
+++ task0101.java
@@ -0,0 +1,8 @@
+/*  Круто быть программистом! */ 
+
+public class Solution {     
+  public static void main(String[] args) {         
+  //напишите тут ваш код         
+  System.out.println ("Я думаю, быть программистом - это круто");    
+  } 
+}
