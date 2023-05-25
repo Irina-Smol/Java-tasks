7136aa4de0ae3bcc94c80cacb5ae95005195c620
Ирина <Smolenskaya2002@yandex.ru>
2023-05-25 21:29:02 +0300

    Add files via upload

---
+++ 1.JavaSyntax/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    //напишите тут ваш код
+    public static void main(String[] args) {
+        printString("Hello Amigo!");
+    }
+
+    public static void printString (String s) {
+        System.out.println(s);
+    }
+}
